5a9aa3130f460f3e2c1b57b6f5a854e081f327cf
bakashev <bakashevs@mail.ru>
2023-04-05 17:22:01 +0300

    Добавил в класс  итератор генерацию слуяайного стоп числа,а в функцию генетрата, аргумент для указания стоп числа при создании объекта

--- hw_lesson13_1.py
+++ hw_lesson13_1.py
@@ -2,6 +2,8 @@ import random
 # 1. Написать бесконечный итератор, который возвращает случайно число в заданном диапазоне.
 # Реализовать через класс-итератор и через функцию-генератор.
 # Протестировать обе реализации в цикле for.
+# Дополнительно (если есть время) добавить параметр стоп-число: как только генератор
+# выдает стоп-число, он заканчивает генерировать новые.
 
 
 class IterableObject:
@@ -19,26 +21,30 @@ class IterableObject:
         if rezult == self.stop_number:
             print(f'Случайное число {rezult} = стоп числу {self.stop_number}')
             raise StopIteration
+
         return rezult
 
 
 first = IterableObject(19, 30)
 print(f'стоп число {first.stop_number}')
 for i in iter(first):
-    item = iter(first)
-    print(next(item))
+    #item = iter(first)
+    print(i)
 
 print('==================================')
 
 
-def generator_object(start_range: int, stop_range: int, start_number: int = None):
-    while True:
-        rezult = random.randint(start_range, stop_range)
-        yield rezult
+def generator_object(start_range: int, stop_range: int, stop_number:int = None):
+    my_rezult = 0
+    while my_rezult != stop_number:
+        my_rezult = random.randint(start_range, stop_range)
+        print(f' стоп число {stop_number}, результат {my_rezult}')
+        yield my_rezult
 
-#second = generator_object(23, 100)
-for i in range(10):
-    print(next(generator_object(23, 100)))
+
+second = generator_object(23, 100, 24)
+for i in iter(second):
+    print(i)
 
 
 
